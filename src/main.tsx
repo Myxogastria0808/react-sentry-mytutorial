6275b8faa4ab2f867d7e8ed1bdd871cde4d19aa8
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-04-04 16:47:59 +0900

    feat: add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,39 @@ import App from "./App.tsx";
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://c76c7633dc62169056e0ec139bb202b0@o4507043919233024.ingest.us.sentry.io/4508874027761664",
+  dsn: `${import.meta.env.VITE_SENTRY_DSN}`,
   integrations: [
     Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
+    // Use the default strategy, an alias for `feedbackSyncIntegration`
+    // https://docs.sentry.io/platforms/javascript/guides/react/user-feedback/configuration/#crash-report-modal
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "light",
+      showName: true,
+      showEmail: true,
+      enableScreenshot: true,
+      isNameRequired: true,
+      isEmailRequired: true,
+      triggerLabel: "不具合を報告",
+      triggerAriaLabel: "不具合を報告",
+      formTitle: "不具合報告フォーム",
+      nameLabel: "お名前",
+      namePlaceholder: "お名前を入力してください",
+      emailLabel: "メールアドレス",
+      emailPlaceholder: "メールアドレスを入力してください",
+      messageLabel: "不具合の詳細",
+      isRequiredLabel: "（必須）",
+      messagePlaceholder: "不具合の詳細を入力してください",
+      addScreenshotButtonLabel: "スクリーンショットを追加",
+      removeScreenshotButtonLabel: "スクリーンショットを削除",
+      submitButtonLabel: "送信",
+      cancelButtonLabel: "キャンセル",
+      successMessageText: "ご協力していただき、ありがとうございました！",
+    }),
   ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost"],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
 });
 
 createRoot(document.getElementById("root")!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "myxogastria0808",
-    project: "react-sentry-mytutorial"
-  })],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
